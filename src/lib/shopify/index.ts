5e1c15864311055b6e5fb102ac43807f362b6a3e
Aadi Golecha <aadigolecha@gmail.com>
2026-07-04 19:25:56 +0530

    chore: migrate to hype26 and shopify storefront api

---
+++ src/lib/shopify/index.ts
@@ -0,0 +1,55 @@
+export const domain = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN || '';
+export const storefrontAccessToken = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN || '';
+export const apiVersion = process.env.SHOPIFY_STOREFRONT_API_VERSION || '2024-01';
+
+type ShopifyFetchParams = {
+  cache?: RequestCache;
+  headers?: HeadersInit;
+  query: string;
+  tags?: string[];
+  variables?: any;
+};
+
+export async function shopifyFetch<T>({
+  cache = 'force-cache',
+  headers,
+  query,
+  tags,
+  variables
+}: ShopifyFetchParams): Promise<{ status: number; body: T } | never> {
+  try {
+    const endpoint = `https://${domain}/api/${apiVersion}/graphql.json`;
+
+    const result = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Shopify-Storefront-Access-Token': storefrontAccessToken,
+        ...headers
+      },
+      body: JSON.stringify({
+        ...(query && { query }),
+        ...(variables && { variables })
+      }),
+      cache,
+      ...(tags && { next: { tags } })
+    });
+
+    const body = await result.json();
+
+    if (body.errors) {
+      throw body.errors[0];
+    }
+
+    return {
+      status: result.status,
+      body
+    };
+  } catch (e) {
+    if (e instanceof Error) {
+      throw e;
+    }
+
+    throw new Error('An unknown error occurred while fetching from Shopify');
+  }
+}
